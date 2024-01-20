9196a33cd844a11df900047885a94baa1e0536a6
jlpureco <101906616+jlpureco@users.noreply.github.com>
2024-01-20 13:40:48 -0600

    Update admon_app.py

--- admon_app.py
+++ admon_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.markdown("<h1 style='text-align: center; color: black;'>LR Travel</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: white;'>LR Travel</h1>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
